bba2e8dccee2ee6968d05a8c22da7a376fc19921
Zheng-Xiang Ye <65601854+yehzx@users.noreply.github.com>
2024-03-04 13:57:33 +0800

    Add full-text feature

--- pubtator3_api.py
+++ pubtator3_api.py
@@ -1,5 +1,6 @@
 # https://www.ncbi.nlm.nih.gov/research/pubtator3/api
 import argparse
+import sys
 import time
 from collections import OrderedDict
 from datetime import datetime
@@ -9,38 +10,59 @@ from typing import Literal
 import requests
 from tqdm.auto import tqdm
 
-# pmid batch_size
-BATCH_SIZE = 100
+# API GET limit: 100
+PMID_REQUEST_SIZE = 100
+QUERY_METHOD = ["search", "cite"][1]
+# Full text annotation is only availabe in `biocxml` and `biocjson` formats
+# RESPONSE_FORMAT = ["pubtator", "biocxml", "biocjson"][2]
 
 
-def run_query_pipeline(query, savepath, type: Literal["query", "pmids"], name):
+def run_query_pipeline(query, savepath, type: Literal["query", "pmids"],
+                       name, max_articles=1000, full_text=False):
     if type == "query":
-        article_dict = get_search_results(query)
-        output = obtain_article_annotations(article_dict)
-        output_path = savepath / f"{query}.pubtator" if name is None else savepath / f"{name}.pubtator"
+        pmid_list = get_search_results(query, max_articles)
+        output = batch_publication_query(
+            pmid_list, type="pmid", full_text=full_text)
+        if full_text:
+            output = [convert_to_pubtator(i) for i in output]
+        output_path = savepath / \
+            f"{query}.pubtator" if name is None else savepath / f"{name}.pubtator"
         write_output(output, savepath=output_path)
     elif type == "pmids":
-        output = batch_publication_query(query, type="pmids")
+        output = batch_publication_query(
+            query, type="pmids", full_text=full_text)
+        if full_text:
+            output = [convert_to_pubtator(i) for i in output]
         now = datetime.now().strftime("%Y%m%d%H%M%S")
-        output_path = savepath / f"result_{now}.pubtator" if name is None else savepath / f"{name}.pubtator"
+        output_path = savepath / \
+            f"result_{now}.pubtator" if name is None else savepath / \
+            f"{name}.pubtator"
         write_output(output, savepath=output_path)
 
 
-def get_search_results(query, max_articles=1000):
+def get_search_results(query, max_articles):
     print(f"Query: {query}")
-    res = send_search_query(query)
+    if QUERY_METHOD == "search":
+        article_list = get_by_search(query, max_articles)
+    elif QUERY_METHOD == "cite":
+        article_list = get_by_cite(query, max_articles)
+
+    return article_list
+
+
+def get_by_search(query, max_articles):
+    res = send_search_query(query, type="search")
+
+    article_list = []
+    if not request_successful(res):
+        return article_list
+
     res_json = res.json()
     total_articles = int(res_json["count"])
-    total_pages = int(res_json["total_pages"])
     page_size = int(res_json["page_size"])
+    article_list.extend(get_article_ids(res_json))
 
-    print(f"Get {total_articles} articles")
-    n_articles_to_request = max_articles if total_articles > max_articles else total_articles
-    print(f"Requesting {n_articles_to_request} articles...")
-
-    article_dict = OrderedDict()
-    if request_successful(res):
-        article_dict.update(get_article_ids(res_json))
+    n_articles_to_request = get_n_articles(max_articles, total_articles)
 
     # Get search results in different pages until the max_articles is reached
     current_page = 1
@@ -50,24 +72,53 @@ def get_search_results(query, max_articles=1000):
             current_page += 1
             res = send_search_query_with_page(query, current_page)
             if request_successful(res):
-                article_dict.update(get_article_ids(res.json()))
+                article_list.extend(get_article_ids(res.json()))
         pbar.n = pbar.total
 
-    while len(article_dict) > max_articles:
-        _ = article_dict.popitem()
-
-    return article_dict
+    return article_list[:n_articles_to_request]
 
 
-def send_search_query(query):
-    search_url = f"https://www.ncbi.nlm.nih.gov/research/pubtator3-api/search/"
+def get_n_articles(max_articles, total_articles):
+    print(f"Get {total_articles} articles")
+    n_articles_to_request = max_articles if total_articles > max_articles else total_articles
+    print(f"Requesting {n_articles_to_request} articles...")
+    return n_articles_to_request
+
+
+def get_by_cite(query, max_articles):
+    res = send_search_query(query, type="cite")
+    if not request_successful(res):
+        return OrderedDict()
+
+    pmid_list = parse_cite_response(res.text)
+    n_articles_to_request = get_n_articles(max_articles, len(pmid_list))
+
+    return pmid_list[:n_articles_to_request]
+
+
+def parse_cite_response(res_text):
+    pmid_list = []
+    for line in res_text.split("\n"):
+        if line.startswith("#") or line == "":
+            continue
+        # [pmid, title, journal]
+        pmid = line.split("\t")[0]
+        pmid_list.append(pmid)
+    return pmid_list
+
+
+def send_search_query(query, type: Literal["search", "cite"] = "cite"):
+    if type == "search":
+        search_url = "https://www.ncbi.nlm.nih.gov/research/pubtator3-api/search/"
+    elif type == "cite":
+        search_url = "https://www.ncbi.nlm.nih.gov/research/pubtator3-api/cite/tsv"
     res = requests.get(search_url, params={"text": query})
     time.sleep(0.5)
     return res
 
 
 def send_search_query_with_page(query, page):
-    search_url = f"https://www.ncbi.nlm.nih.gov/research/pubtator3-api/search/"
+    search_url = "https://www.ncbi.nlm.nih.gov/research/pubtator3-api/search/"
     res = requests.get(search_url, params={"text": query,
                                            "sort": "score desc",
                                            "page": page})
@@ -83,56 +134,105 @@ def request_successful(res):
 
 
 def get_article_ids(res_json):
-    article_dict = {}
-    for article in res_json["results"]:
-        article_id = article["_id"]
-        article_dict[article_id] = {}
-        # Get pmid
-        article_dict[article_id]["pmid"] = article.get("pmid")
-        # Get pmcid
-        article_dict[article_id]["pmcid"] = article.get("pmcid")
-
-    return article_dict
+    return [article.get("pmid") for article in res_json["results"]]
 
 
-def obtain_article_annotations(article_dict, full_text=False):
+def batch_publication_query(id_list, type, full_text=False):
     output = []
-    print("Obtaining article annotations...")
-    if full_text:
-        pass
-    else:
-        to_query_pmids = []
-        for article in article_dict.values():
-            to_query_pmids.append(str(article["pmid"]))
-
-        output = batch_publication_query(to_query_pmids, type="pmids")
-
-    return output
-
-
-def batch_publication_query(id_list, type, batch_size=BATCH_SIZE, format="pubtator"):
-    output = []
+    format = "biocjson" if full_text else "pubtator"
     with tqdm(total=len(id_list)) as pbar:
-        for start in range(0, len(id_list), batch_size):
-            end = start + batch_size 
+        for start in range(0, len(id_list), PMID_REQUEST_SIZE):
+            end = start + PMID_REQUEST_SIZE
             end = end if end < len(id_list) else None
             res = send_publication_query(
-                ",".join(id_list[start:end]), type=type, format=format)
+                ",".join(id_list[start:end]), type=type, format=format, full_text=full_text)
             if request_successful(res):
-                output.append(res.text)
-            if end is not None: 
-                pbar.update(batch_size)
+                output.append(res.json() if full_text else res.text)
+            if end is not None:
+                pbar.update(PMID_REQUEST_SIZE)
             else:
                 pbar.n = len(id_list)
 
     return output
 
 
-def send_publication_query(article_id, type, format="pubtator"):
+def send_publication_query(article_id, type: Literal["pmids", "pmcids"], format, full_text=False):
     pub_url = f"https://www.ncbi.nlm.nih.gov/research/pubtator3-api/publications/export/{format}"
-    res = requests.get(pub_url, params={type: article_id})
+    params = {type: article_id}
+    if full_text:
+        params["full"] = "true"
+    res = requests.get(pub_url, params=params)
     time.sleep(0.5)
     return res
+
+
+def convert_to_pubtator(res_json):
+    pmid = res_json["pmid"]
+    title = res_json["passages"][0]["text"]
+    annotation_list = get_biocjson_annotations(res_json)
+    relation_list = get_biocjson_relations(res_json)
+    converted_str = create_pubtator_str(
+        pmid, title, annotation_list, relation_list)
+
+    return converted_str
+
+
+def create_pubtator_str(pmid, title, annotation_list, relation_list):
+    title_str = f"{pmid}|t|{title}\n"
+    annotation_str = [(f"{pmid}\t"
+                       f"{annotation['locations']['offset']}\t"
+                       f"{annotation['locations']['length'] + annotation['locations']['offset']}\t"
+                       f"{annotation['name']}\t"
+                       f"{annotation['type']}\t"
+                       f"{annotation['id']}")
+                      for annotation in annotation_list]
+    relation_str = [(f"{pmid}\t"
+                     f"{relation['type']}\t"
+                     f"{relation['role1']}\t"
+                     f"{relation['role2']}")
+                    for relation in relation_list]
+
+    return title_str + "\n".join(annotation_str) + "\n" + "\n".join(relation_str)
+
+
+def get_biocjson_annotations(res_json):
+    n_passages = len(res_json["passages"])
+    passages_type = [res_json["passages"][i]["infons"]["type"]
+                     for i in range(n_passages)]
+    annotation_list = []
+    # TODO: extract from specific passages only?
+    for annotation_entries in [res_json["passages"][i]["annotations"] for i in range(n_passages)]:
+        for annotation_entry in annotation_entries:
+            each_annotation = {}
+            each_annotation["id"] = annotation_entry["infons"]["normalized_id"]
+            each_annotation["type"] = annotation_entry["infons"]["type"]
+            each_annotation["locations"] = annotation_entry["locations"][0]
+
+            # In type == "species", the entity name is stored in "text"
+            if each_annotation["type"] == "Species":
+                each_annotation["name"] = annotation_entry["text"]
+            else:
+                try:
+                    each_annotation["name"] = annotation_entry["infons"]["name"]
+                except KeyError:
+                    each_annotation["name"] = annotation_entry["text"]
+
+            annotation_list.append(each_annotation)
+
+    return annotation_list
+
+
+def get_biocjson_relations(res_json):
+    n_relations = len(res_json["relations"])
+    relation_list = []
+    for relation_entry in res_json["relations"]:
+        each_relation = {}
+        each_relation["role1"] = relation_entry["infons"]["role1"]["identifier"]
+        each_relation["role2"] = relation_entry["infons"]["role2"]["identifier"]
+        each_relation["type"] = relation_entry["infons"]["type"]
+        relation_list.append(each_relation)
+
+    return relation_list
 
 
 def write_output(output, savepath: Path):
@@ -181,6 +281,8 @@ if __name__ == "__main__":
                         help="Filepath to load PMIDs")
     parser.add_argument("--max_articles", type=int, default=1000,
                         help="Maximal articles to request from the searching result (default: 1000)")
+    parser.add_argument("--full_text", action="store_true",
+                        help="Get full-text annotations")
     args = parser.parse_args()
 
     output_dir = Path(args.output)
@@ -188,12 +290,23 @@ if __name__ == "__main__":
         raise Exception(f"{output_dir} is not a directory")
 
     if args.query is not None:
-        run_query_pipeline(args.query, output_dir, "query", args.name)
+        run_query_pipeline(query=args.query,
+                           savepath=output_dir,
+                           type="query",
+                           name=args.name,
+                           max_articles=args.max_articles,
+                           full_text=args.full_text)
+        sys.exit()
     elif args.pmids is not None:
         pmids = args.pmids.split(",")
         pmids = drop_if_not_num(pmids)
         print(f"Find {len(pmids)} PMIDs")
-        run_query_pipeline(pmids, output_dir, "pmids", args.name)
     elif args.pmid_file is not None:
         pmids = load_pmids(args.pmid_file)
-        run_query_pipeline(pmids, output_dir, "pmids", args.name)
+
+    run_query_pipeline(query=pmids,
+                       savepath=output_dir,
+                       type="pmids",
+                       name=args.name,
+                       max_articles=args.max_articles,
+                       full_text=args.full_text)
