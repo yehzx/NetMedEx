fe522ca2736d30ad78441a1251d386d3ed8f74f9
Zheng-Xiang Ye <65601854+yehzx@users.noreply.github.com>
2024-10-16 21:02:28 +0800

    Restructure cytoscape json generation

--- pubtoscape/cytoscape_js.py
+++ pubtoscape/cytoscape_js.py
@@ -1,20 +1,32 @@
-import networkx as nx
 import json
+from typing import Literal
+
+import networkx as nx
+
+from pubtoscape.cytoscape_html_template import HTML_TEMPLATE
+
+
+def save_as_html(G: nx.Graph, savepath: str, layout="preset"):
+    with open(savepath, "w") as f:
+        cytoscape_js = create_cytoscape_js(G, style="cyjs")
+        f.write(HTML_TEMPLATE.format(cytoscape_js=json.dumps(cytoscape_js),
+                                     layout=layout))
 
 
 def save_as_json(G: nx.Graph, savepath: str):
     with open(savepath, "w") as f:
-        cytoscape_js = create_cytoscape_json(G)
+        cytoscape_js = create_cytoscape_js(G, style="dash")
         f.write(json.dumps(cytoscape_js))
 
 
-def create_cytoscape_json(G: nx.Graph):
-    elements = {"elements": {"nodes": [], "edges": []}}
-    for node in G.nodes(data=True):
-        elements["elements"]["nodes"].append(create_cytoscape_node(node))
+def create_cytoscape_js(G: nx.Graph, style: Literal["dash", "cyjs"] = "cyjs"):
+    nodes = [create_cytoscape_node(node) for node in G.nodes(data=True)]
+    edges = [create_cytoscape_edge(edge, G) for edge in G.edges(data=True)]
 
-    for edge in G.edges(data=True):
-        elements["elements"]["edges"].append(create_cytoscape_edge(edge, G))
+    if style == "cyjs":
+        elements = nodes + edges
+    elif style == "dash":
+        elements = {"elements": {"nodes": nodes, "edges": edges}}
 
     return elements
 
@@ -27,8 +39,8 @@ def create_cytoscape_node(node):
             "id": node_attr["_id"],
             "parent": node_attr.get("parent", None),
             "color": node_attr["color"],
-            "label": node_attr["name"],
             "label_color": node_attr["label_color"],
+            "label": node_attr["name"],
             "shape": node_attr["shape"].lower(),
         },
         "position": {
@@ -44,13 +56,11 @@ def create_cytoscape_node(node):
     return node_info
 
 
-def create_cytoscape_edge(edge, G):
+def create_cytoscape_edge(edge, G, with_id=True):
     node_id_1, node_id_2, edge_attr = edge
 
     edge_info = {
         "data": {
-            # Add this will sometimes cause dash_cytoscape to throw node source or target not found
-            # "id": edge_attr["_id"],
             "source": G.nodes[node_id_1]["_id"],
             "target": G.nodes[node_id_2]["_id"],
             "label": f"{G.nodes[node_id_1]['name']} (interacts with) {G.nodes[node_id_2]['name']}",
@@ -58,5 +68,8 @@ def create_cytoscape_edge(edge, G):
             "pmids": edge_attr["pmids"],
         }
     }
+
+    if with_id:
+        edge_info["data"]["id"] = edge_attr["_id"]
 
     return edge_info
