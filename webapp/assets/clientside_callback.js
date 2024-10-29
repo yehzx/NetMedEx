75c624586e2cdbc92860cf89bcd483874c03eab8
Zheng-Xiang Ye <65601854+yehzx@users.noreply.github.com>
2024-10-29 17:36:15 +0800

    Change edge_info to bottom

--- webapp/assets/clientside_callback.js
+++ webapp/assets/clientside_callback.js
@@ -1,8 +1,6 @@
 window.dash_clientside = window.dash_clientside || {}
 window.dash_clientside.clientside = {
-  show_edge_info: function(selected_edges, tap_edge) {
-    console.log(tap_edge)
-    console.log(selected_edges)
+  show_edge_info: function (selected_edges, tap_edge) {
     function check_if_selected(tap_edge) {
       for (let i = 0; i < selected_edges.length; i++) {
         if (selected_edges[i].id === tap_edge.id) {
@@ -12,44 +10,115 @@ window.dash_clientside.clientside = {
       return false
     }
 
+    function get_z_index(visibility) {
+      return visibility === "hidden" ? -100 : 100
+    }
+
     let elements = []
     let visibility = "hidden"
 
     if (tap_edge !== undefined) {
       if (!check_if_selected(tap_edge)) {
-        return [{"visibility": visibility}, elements]
+        return [{ "visibility": visibility, "zIndex": get_z_index(visibility) }, elements]
       }
 
       const pubtator_href = "https://www.ncbi.nlm.nih.gov/research/pubtator3/publication/"
       const [node_1, node_2] = tap_edge.label.split(" (interacts with) ")
 
-      elements.push({props: {children: `Node 1: ${node_1}`}, type: "P", namespace: "dash_html_components"})
-      elements.push({props: {children: `Node 2: ${node_2}`}, type: "P", namespace: "dash_html_components"})
-      elements.push({props: {children: "Evidence:"}, type: "P", namespace: "dash_html_components"})
+      elements.push({ props: { children: `Node 1: ${node_1}` }, type: "P", namespace: "dash_html_components" })
+      elements.push({ props: { children: `Node 2: ${node_2}` }, type: "P", namespace: "dash_html_components" })
+      // elements.push({ props: { children: "Evidence:" }, type: "P", namespace: "dash_html_components" })
+      const edge_table = {
+        type: "Table",
+        namespace: "dash_html_components",
+        props: {
+          className: "table table-bordered table-striped table-sm",
+          children: [
+            {
+              type: "Thead",
+              namespace: "dash_html_components",
+              props: {
+                children: [
+                  {
+                    type: "Tr",
+                    namespace: "dash_html_components",
+                    props: {
+                      children: [
+                        {
+                          type: "Th",
+                          namespace: "dash_html_components",
+                          props: {
+                            children: "No.",
+                          }
+                        },
+                        {
+                          type: "Th",
+                          namespace: "dash_html_components",
+                          props: {
+                            children: "PMID",
+                          }
+                        },
+                        {
+                          type: "Th",
+                          namespace: "dash_html_components",
+                          props: {
+                            children: "Title",
+                          }
+                        }
+                      ]
+                    }
+                  }
+                ]
+              }
+            },
+            {
+              type: "Tbody",
+              namespace: "dash_html_components",
+              props: {
+                children: [],
+              }
+            }
+          ]
+        }
+      }
+
+      const table_entry = edge_table.props.children[1].props.children
 
       Object.entries(tap_edge.pmids).forEach((obj, index) => {
         const pmid = obj[0]
         const title = obj[1]
-        elements.push({
-          type: "P",
+        table_entry.push({
+          type: "Tr",
           namespace: "dash_html_components",
           props: {
             children: [
-              `${index + 1}. `,
               {
-                type: "A",
+                type: "Td",
                 namespace: "dash_html_components",
                 props: {
-                  href: pubtator_href + pmid,
-                  target: "_blank",
-                  children: pmid
+                  children: `${index + 1}`,
                 }
               },
               {
-                type: "P",
+                type: "Td",
                 namespace: "dash_html_components",
                 props: {
-                  children: `${title}`
+                  children: {
+                    type: "A",
+                    namespace: "dash_html_components",
+                    props: {
+                      href: pubtator_href + pmid,
+                      target: "_blank",
+                      children: pmid,
+                    }
+                  }
+                }
+              },
+              {
+                type: "Td",
+                namespace: "dash_html_components",
+                props: {
+                  children: `${title}`,
                 }
               }
             ]
@@ -58,9 +127,10 @@ window.dash_clientside.clientside = {
       })
 
       visibility = "visible"
+      elements.push(edge_table)
     }
-    console.log(elements)
-    
-    return [{"visibility": visibility}, elements]
+
+
+    return [{ "visibility": visibility, "zIndex": get_z_index(visibility) }, elements]
   }
 }
